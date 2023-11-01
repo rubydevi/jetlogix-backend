b76e58b4e43cae7f1aa136f677e5ad34377195ac
iamrubydevi <ariesabyss@gmail.com>
2023-11-01 18:15:59 +0530

    Validate all the necessary fields

--- app/models/aeroplane.rb
+++ app/models/aeroplane.rb
@@ -1,3 +1,7 @@
 class Aeroplane < ApplicationRecord
   has_many :reservation, dependent: :destroy
+
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :fee, numericality: { greater_than_or_equal_to: 0.0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_many :reservations, foreign_key: 'user_id', dependent: :destroy
+
+  validates :name, presence: true
 end
