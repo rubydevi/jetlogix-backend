6323b43292b9069d8546f3a01d1f489358308e22
iamrubydevi <ariesabyss@gmail.com>
2023-11-01 22:06:21 +0530

    Add some dummy data to test

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# Create a dummy User
+User.create(name: 'John')
+
+# Create some sample aeroplanes
+Aeroplane.create(name: 'Aeroplane 1', model: 'jet', image: 'image1', description: 'yolo', number_of_seats: 100, location: 'test', fee: 90.0, reserved: true)
+Aeroplane.create(name: 'Aeroplane 2', model: 'jet', image: 'image1', description: 'yolo', number_of_seats: 100, location: 'test', fee: 90.0, reserved: true)
+Aeroplane.create(name: 'Aeroplane 3', model: 'jet', image: 'image1', description: 'yolo', number_of_seats: 100, location: 'test', fee: 90.0, reserved: true)
