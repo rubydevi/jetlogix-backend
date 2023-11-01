9c7d5b9fe6a3d3ba7b93f9d82d898573c776c47a
Yonashenok <yonashenok24@gmail.com>
2023-11-01 17:58:05 +0300

    feat: respond_with using jsonapi_serializers

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -304,22 +304,22 @@ Devise.setup do |config|
   # Note: These might become the new default in future versions of Devise.
   config.responder.error_status = :unprocessable_entity
   config.responder.redirect_status = :see_other
-
   # ==> Configuration for :registerable
 
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
   # Rails.application.credentials.fetch(:secret_key_base)
+  # Rails.application.credentials.secret_key_base
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.secret_key_base
+    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
     jwt.dispatch_requests = [
       ['POST', %r{^/login$}]
     ]
     jwt.revocation_requests = [
       ['DELETE', %r{^/logout$}]
     ]
-    jwt.expiration_time = 50.minutes.to_i
+    jwt.expiration_time = 30.minutes.to_i
   end
 end
