ea885d4f4de422773401540daee7553e1016c8e1
iamrubydevi <ariesabyss@gmail.com>
2023-11-01 15:53:11 +0530

    Fix Linters

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,5 @@
 class ReservationsController < ApplicationController
-  before_action :set_reservation, only: %i[ show update destroy ]
+  before_action :set_reservation, only: %i[show update destroy]
 
   # GET /reservations
   def index
@@ -39,13 +39,15 @@ class ReservationsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_reservation
-      @reservation = Reservation.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def reservation_params
-      params.require(:reservation).permit(:reserved_date, :start_time, :end_time, :total_cost, :start_location, :destination, :user_id, :aeroplane_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def reservation_params
+    params.require(:reservation).permit(:reserved_date, :start_time, :end_time, :total_cost, :start_location,
+                                        :destination, :user_id, :aeroplane_id)
+  end
 end
--- spec/routing/aeroplanes_routing_spec.rb
+++ spec/routing/aeroplanes_routing_spec.rb
@@ -1,30 +1,29 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe AeroplanesController, type: :routing do
-  describe "routing" do
-    it "routes to #index" do
-      expect(get: "/aeroplanes").to route_to("aeroplanes#index")
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/aeroplanes').to route_to('aeroplanes#index')
     end
 
-    it "routes to #show" do
-      expect(get: "/aeroplanes/1").to route_to("aeroplanes#show", id: "1")
+    it 'routes to #show' do
+      expect(get: '/aeroplanes/1').to route_to('aeroplanes#show', id: '1')
     end
 
-
-    it "routes to #create" do
-      expect(post: "/aeroplanes").to route_to("aeroplanes#create")
+    it 'routes to #create' do
+      expect(post: '/aeroplanes').to route_to('aeroplanes#create')
     end
 
-    it "routes to #update via PUT" do
-      expect(put: "/aeroplanes/1").to route_to("aeroplanes#update", id: "1")
+    it 'routes to #update via PUT' do
+      expect(put: '/aeroplanes/1').to route_to('aeroplanes#update', id: '1')
     end
 
-    it "routes to #update via PATCH" do
-      expect(patch: "/aeroplanes/1").to route_to("aeroplanes#update", id: "1")
+    it 'routes to #update via PATCH' do
+      expect(patch: '/aeroplanes/1').to route_to('aeroplanes#update', id: '1')
     end
 
-    it "routes to #destroy" do
-      expect(delete: "/aeroplanes/1").to route_to("aeroplanes#destroy", id: "1")
+    it 'routes to #destroy' do
+      expect(delete: '/aeroplanes/1').to route_to('aeroplanes#destroy', id: '1')
     end
   end
 end
