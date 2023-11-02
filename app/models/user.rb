ec602884b6867d1a7e60eef34076080bd6d53521
Yonashenok <yonashenok24@gmail.com>
2023-11-02 08:15:32 +0300

    fix: linter error

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-        :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+         :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
   has_many :reservations, foreign_key: 'user_id', dependent: :destroy
 
   validates :name, presence: true
