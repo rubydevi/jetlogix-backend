d9c1b9954a57d52c2feeadbd6e32c30f256ed82f
iamrubydevi <ariesabyss@gmail.com>
2023-11-03 00:30:06 +0530

    Dummy data for testing this PR

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,6 @@
 User.create(name: 'John')
 
 # Create some sample aeroplanes
-Aeroplane.create(name: 'Aeroplane 1', model: 'jet', image: 'image1', description: 'yolo', number_of_seats: 100, location: 'test', fee: 90.0, reserved: true)
-Aeroplane.create(name: 'Aeroplane 2', model: 'jet', image: 'image1', description: 'yolo', number_of_seats: 100, location: 'test', fee: 90.0, reserved: true)
-Aeroplane.create(name: 'Aeroplane 3', model: 'jet', image: 'image1', description: 'yolo', number_of_seats: 100, location: 'test', fee: 90.0, reserved: true)
+Aeroplane.create(name: 'Aeroplane 1', model: 'https://images.unsplash.com/photo-1464037866556-6812c9d1c72e?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', image: 'image1', description: 'yolo', number_of_seats: 100, location: 'test', fee: 90.0, reserved: true)
+Aeroplane.create(name: 'Aeroplane 2', model: 'https://images.unsplash.com/photo-1464037866556-6812c9d1c72e?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', image: 'image1', description: 'yolo', number_of_seats: 100, location: 'test', fee: 90.0, reserved: true)
+Aeroplane.create(name: 'Aeroplane 3', model: 'https://images.unsplash.com/photo-1464037866556-6812c9d1c72e?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', image: 'image1', description: 'yolo', number_of_seats: 100, location: 'test', fee: 90.0, reserved: true)
