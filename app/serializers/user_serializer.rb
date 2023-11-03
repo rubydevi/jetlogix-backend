5b1ca65d6947269dc9fecbd67b37f8bb61b0d303
iamrubydevi <ariesabyss@gmail.com>
2023-11-03 17:35:11 +0530

    Add role in user_serialiser

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,6 @@
 class UserSerializer
   include JSONAPI::Serializer
-  attributes :id, :email, :created_at, :name
+  attributes :id, :email, :created_at, :name, :role
 
   attribute :created_date do |user|
     user.created_at&.strftime('%m/%d/%Y')
