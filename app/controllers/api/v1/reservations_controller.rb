a7e7b4d2774b2143c371029ae2ab18053d6e28b1
Yonashenok <yonashenok24@gmail.com>
2023-11-04 12:09:51 +0300

    feat: add before action to auth

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ReservationsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   before_action :set_reservation, only: %i[show update destroy]
 
   # GET /reservations
