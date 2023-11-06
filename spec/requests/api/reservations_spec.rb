209b13f833f6e072f00197f70f4a2a5572e65f37
RileyManda <rileymanda69@gmail.com>
2023-11-06 12:33:16 +0200

    Uppdated reservations specs

--- spec/requests/api/reservations_spec.rb
+++ spec/requests/api/reservations_spec.rb
@@ -4,18 +4,21 @@ RSpec.describe '/reservations', type: :request do
   # This should return the minimal set of attributes required to create a valid
   # Reservation. As you add validations to Reservation, be sure to
   # adjust the attributes here as well.
-  let(:reservation) do
-    skip('Add a hash of attributes valid for your model')
-  end
-
-  let(:reservation) do
-    skip('Add a hash of attributes invalid for your model')
-  end
-
   # This should return the minimal set of values that should be in the headers
   # in order to pass any filters (e.g. authentication) defined in
   # ReservationsController, or in your router and rack
   # middleware. Be sure to keep this updated too.
+    let(:user) do
+    {
+      id: 3,
+      name: 'Reservation 3',
+      email: 'ri@gmail.com',
+      password: 'https://images.unsplash.com/photo-1464037866556-6812c9d1c72e?auto=format&fit=crop&q=80&w=2070&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      role: 'user'
+
+    }
+  end
+
   let(:reservation) do
     {
       reserved_date: '2021-04-20',
@@ -31,94 +34,57 @@ RSpec.describe '/reservations', type: :request do
 
   describe 'GET /index' do
     it 'renders a successful response' do
+      path '/reservations' do
       Reservation.create! reservation
-      get reservations_url, headers: valid_headers, as: :json
+      get api_v1_user_reservations_path, as: :json
       expect(response).to be_successful
     end
   end
+end
 
-  describe 'GET /show' do
-    it 'renders a successful response' do
-      reservation = Reservation.create! reservation
-      get reservation_url(reservation), as: :json
-      expect(response).to be_successful
-    end
-  end
 
   describe 'POST /create' do
     context 'with valid parameters' do
       it 'creates a new Reservation' do
+         path '/reservations' do
         expect do
-          post reservations_url,
-               params: { reservation: }, headers: valid_headers, as: :json
+          post api_v1_user_reservations_path,
+               params: { reservation: }, as: :json
         end.to change(Reservation, :count).by(1)
       end
+    end
 
       it 'renders a JSON response with the new reservation' do
-        post reservations_url,
-             params: { reservation: }, headers: valid_headers, as: :json
+         path '/reservations' do
+        post api_v1_user_reservations_path,
+             params: { reservation: },as: :json
         expect(response).to have_http_status(:created)
         expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
+  end
 
     context 'with invalid parameters' do
       it 'does not create a new Reservation' do
+         path '/reservations' do
         expect do
-          post reservations_url,
+          post api_v1_user_reservations_path,
                params: { reservation: reserve }, as: :json
         end.to change(Reservation, :count).by(0)
       end
+    end
 
       it 'renders a JSON response with errors for the new reservation' do
-        post reservations_url,
-             params: { reservation: reserving }, headers: valid_headers, as: :json
+         path '/reservations' do
+        post api_v1_user_reservations_path,
+             params: { reservation: reserving }, as: :json
         expect(response).to have_http_status(:unprocessable_entity)
         expect(response.content_type).to match(a_string_including('application/json'))
       end
     end
   end
 
-  describe 'PATCH /update' do
-    context 'with valid parameters' do
-      let(:new_attributes) do
-        skip('Add a hash of attributes valid for your model')
-      end
 
-      it 'updates the requested reservation' do
-        reservation = Reservation.create! reservation
-        patch reservation_url(reservation),
-              params: { reservation: new_attributes }, headers: valid_headers, as: :json
-        reservation.reload
-        skip('Add assertions for updated state')
-      end
-
-      it 'renders a JSON response with the reservation' do
-        reservation = Reservation.create! reservation
-        patch reservation_url(reservation),
-              params: { reservation: new_attributes }, headers: valid_headers, as: :json
-        expect(response).to have_http_status(:ok)
-        expect(response.content_type).to match(a_string_including('application/json'))
-      end
-    end
-
-    context 'with invalid parameters' do
-      it 'renders a JSON response with errors for the reservation' do
-        reservation = Reservation.create! reservation
-        patch reservation_url(reservation),
-              params: { reservation: minions }, headers: valid_headers, as: :json
-        expect(response).to have_http_status(:unprocessable_entity)
-        expect(response.content_type).to match(a_string_including('application/json'))
-      end
-    end
-  end
-
-  describe 'DELETE /destroy' do
-    it 'destroys the requested reservation' do
-      reservation = Reservation.create! reservation
-      expect do
-        delete reservation_url(reservation), headers: valid_headers, as: :json
-      end.to change(Reservation, :count).by(-1)
-    end
   end
 end
+
